871a8e956f5c8208ff3729bb9a03f237748b0554
Adrian <ad.dever@yahoo.com>
2024-05-24 10:16:41 +0200

    updated cors

--- auth.js
+++ auth.js
@@ -17,6 +17,7 @@ module.exports = (router) => {
   router.post("/login", (req, res) => {
     passport.authenticate("local", { session: false }, (error, user, info) => {
       if (error || !user) {
+        console.log("Authentication error or user not found:", error);
         return res.status(400).json({
           message: "Something is not right",
           user: user,
@@ -24,9 +25,11 @@ module.exports = (router) => {
       }
       req.login(user, { session: false }, (error) => {
         if (error) {
+          console.log("Login error:", error);
           res.send(error);
         }
         let token = generateJWTToken(user.toJSON());
+        console.log("Generated JWT token:", token);
         return res.json({ user, token });
       });
     })(req, res);
