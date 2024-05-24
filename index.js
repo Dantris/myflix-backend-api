c6366b985097e2b3116c89982cc594792181aef9
Adrian <ad.dever@yahoo.com>
2024-05-25 01:30:24 +0200

    updated

--- index.js
+++ index.js
@@ -39,19 +39,6 @@ app.use(express.static("public"));
 // ];
 // Define CORS options
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        let message = `The CORS policy for this application doesn’t allow access from origin ${origin}`;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
-
 let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
