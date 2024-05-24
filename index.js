a433b8245697f503d8e670ec7864f833add99ba7
Adrian <ad.dever@yahoo.com>
2024-05-24 12:10:02 +0200

    updated

--- index.js
+++ index.js
@@ -38,22 +38,22 @@ let auth = require("./auth")(app);
 const passport = require("passport");
 require("./passport");
 
-// mongoose
-//   .connect("mongodb://localhost:27017/myflixv1", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to the database");
-//   })
-//   .catch((err) => {
-//     console.error("Database connection error:", err);
-//   });
+mongoose
+  .connect("mongodb://localhost:27017/myflixv1", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
 
-mongoose.connect(process.env.CONNECTION_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect(process.env.CONNECTION_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
